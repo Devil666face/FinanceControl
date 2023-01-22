b51961c043d243019661d195510a25f87615b48f
Devil666face <artem1999k@gmail.com>
2023-01-22 10:26:39 +0300

    deploy and start

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,10 +1,24 @@
 import os
-from config.settings import STATIC_ROOT, MEDIA_ROOT
+import socket
+from config.settings import (
+    STATIC_ROOT, 
+    MEDIA_ROOT,
+    DEBUG,
+)
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+host = socket.gethostbyname(socket.gethostname())
+print(f'Server start on http://{host}')
 application = get_wsgi_application()
 application = WhiteNoise(application, root=STATIC_ROOT, prefix='static/')
 application.add_files(root=MEDIA_ROOT, prefix="media/")
+if not DEBUG:
+    from sys import platform
+    if platform == "linux" or platform == "linux2":
+        os.system('ifconfig|grep inet')
+    elif platform == "win32":
+        os.system('ipconfig|findstr IPv4')
+        os.system(f'start http://{host}')
